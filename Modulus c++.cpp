2b3d2b111282be0aa3be1371da7931d2a276db09
adnanaw_ <adnanaw1080@gmail.com>
2023-08-22 03:14:45 +0700

    first commit

---
+++ Modulus c++.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main (){
+	
+	int a, juml;
+	
+	// program modulus % pembagian 3
+	// ex. 10 dibagi 3 = 3 dengan sisa nilai 1
+	
+	cout<< "masukkan nilai nim : " ;
+	cin >> a;
+	cout << endl;
+	
+	juml = a%3;
+	
+	cout <<"hasil : " << juml;
+	cout << endl;
+	cout << endl;
+	cout<< "follow me adnanaw_ on ig";
+	return 0;
+}
